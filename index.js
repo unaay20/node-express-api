4f640439c013fc4a21a14051ccb9b2ca52da1154
mauricinio7 <miory.salas@hotmail.com>
2025-02-28 12:11:54 -0600

    "Implementado endpoint '/saludo/:nombre'"

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
+app.use(express.static(__dirname));
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('Bienvenido a la API');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/saludo/:nombre', (req, res) => {
     res.send(`Hola, ${req.params.nombre}!`);
