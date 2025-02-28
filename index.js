85a35b0aabac6051fbc6bcd882e135a834b31d35
King-Navi <alipadilla03.gmail.com>
2025-02-28 12:18:53 -0600

    Implemnetado endpoint '/mensaje'

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const port = 3000;
 app.use(express.static(__dirname));
 app.use(express.json());
+
+app.use((req, res , next) => {
+    console.log('Soy el hombre de en medio');
+    next();
+});
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
